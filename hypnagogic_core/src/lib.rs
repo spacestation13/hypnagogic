dce617b99602f4e6dc9bd1da80f2556f7269d571
LemonInTheDark <58055496+LemonInTheDark@users.noreply.github.com>
2024-04-25 19:47:42 -0700

    Bumps fixed-map version so syn conflicting version goes away

--- hypnagogic_core/src/lib.rs
+++ hypnagogic_core/src/lib.rs
@@ -5,16 +5,15 @@
 #![allow(clippy::cast_lossless)]
 #![allow(clippy::cast_precision_loss)]
 #![allow(clippy::cast_possible_truncation)]
-// Not actually going to be a published crate, useless to add
+// not actually going to be a published crate, useless to add
 #![allow(clippy::cargo_common_metadata)]
-// Annoying
+// annoying
 #![allow(clippy::module_name_repetitions)]
 // allow this for now, but it's probably a bad idea
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::missing_panics_doc)]
 // sign conversion is fine
 #![allow(clippy::cast_sign_loss)]
-
 pub mod config;
 pub mod generation;
 pub mod operations;
